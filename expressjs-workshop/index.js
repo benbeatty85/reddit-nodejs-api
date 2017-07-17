d9a109506a2eed0650faa1c494edcfa83ca82696
Ben Beatty <benbeatty85@gmail.com>
2017-07-17 22:25:10 +0000

    Finished exercise 3

--- expressjs-workshop/index.js
+++ expressjs-workshop/index.js
@@ -1,15 +1,51 @@
 var express = require('express');
 var app = express();
 
-app.get('/hello', function (req, res) {
-  var name = req.query.name;
+// app.get('/hello', function (req, res) {
+//   var name = req.query.name;
   
-  if (name) {
-    res.send('<h1> Hello ' + name + '!</h1>');
-  } else {
-    res.send('<h1>Hello World!</h1>');
+//   if (name) {
+//     res.send('<h1> Hello ' + name + '!</h1>');
+//   } else {
+//     res.send('<h1>Hello World!</h1>');
+//   }
+// });
+
+app.get('/calculator/:operation', function (req, res) {
+  
+  var num1 = 0;
+  var num2 = 0;
+  if (req.query.num1 !== undefined) {
+    num1 = Number(req.query.num1);
   }
+   
+  if (req.query.num2 !== undefined) {
+    num2 = Number(req.query.num2);
+  }
+   
+  var newOp = req.params.operation;  //newOp is "new operation"
+  var solution;
+   
+  if (newOp == "add") {
+    solution = num1 + num2;
+  } else if (newOp == "multiply") {
+    solution = num1 * num2;
+  } else {
+    res.status(400).send("Error 400: This was a bad request!");
+  }
+   
+     
+    var newOpObject = {
+      "operation": newOp,
+      "firstOperand": num1,
+      "secondOperand": num2,
+      "solution": solution
+    };
+     
+    res.end(JSON.stringify(newOpObject, null, 2));
 });
+  
+
 
 
 
