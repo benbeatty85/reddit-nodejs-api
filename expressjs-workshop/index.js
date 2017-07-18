d8f4df62093e86b78b54804046779e3dfe22bc7b
Ben Beatty <benbeatty85@gmail.com>
2017-07-18 01:50:03 +0000

    Finished exercise 5

--- expressjs-workshop/index.js
+++ expressjs-workshop/index.js
@@ -26,53 +26,74 @@ app.get('/hello', function (req, res) {
   if (name) {
     res.send('<h1> Hello ' + name + '!</h1>');
   } else {
-    res.send('<h1>Hello World!</h1>' + '<p>Bye bitches</p>');
+    res.send('<h1>Hello World!</h1>');
   }
 });
 
 
 //Exercise 3
 
-//Below is how to use the calculator in the link
-//https://reddit-nodejs-project-benbeatty85.c9users.io/calculator/add?num1=3&num2=5
-//https://reddit-nodejs-project-benbeatty85.c9users.io/calculator/multiply?num1=3&num2=5
+// Below is how to use the calculator in the link
+// https://reddit-nodejs-project-benbeatty85.c9users.io/calculator/add?num1=3&num2=5
+// https://reddit-nodejs-project-benbeatty85.c9users.io/calculator/multiply?num1=3&num2=5
 
-// app.get('/calculator/:operation', function (req, res) {
+app.get('/calculator/:operation', function (req, res) {
   
-//   var num1 = 0;
-//   var num2 = 0;
-//   if (req.query.num1 !== undefined) {
-//     num1 = Number(req.query.num1);
-//   }
+  var num1 = 0;
+  var num2 = 0;
+  if (req.query.num1 !== undefined) {
+    num1 = Number(req.query.num1);
+  }
    
-//   if (req.query.num2 !== undefined) {
-//     num2 = Number(req.query.num2);
-//   }
+  if (req.query.num2 !== undefined) {
+    num2 = Number(req.query.num2);
+  }
    
-//   var newOp = req.params.operation;  //newOp is "new operation"
-//   var solution;
+  var newOp = req.params.operation;  //newOp is "new operation"
+  var solution;
    
-//   if (newOp == "add") {
-//     solution = num1 + num2;
-//   } else if (newOp == "multiply") {
-//     solution = num1 * num2;
-//   } else {
-//     res.status(400).send("Error 400: This was a bad request!");
-//   }
+  if (newOp == "add") {
+    solution = num1 + num2;
+  } else if (newOp == "multiply") {
+    solution = num1 * num2;
+  } else {
+    res.status(400).send("Error 400: This was a bad request!");
+  }
    
      
-//     var newOpObject = {
-//       "operation": newOp,
-//       "firstOperand": num1,
-//       "secondOperand": num2,
-//       "solution": solution
-//     };
+    var newOpObject = {
+      "operation": newOp,
+      "firstOperand": num1,
+      "secondOperand": num2,
+      "solution": solution
+    };
      
-//     res.end(JSON.stringify(newOpObject)); 
-// });
+    res.end(JSON.stringify(newOpObject)); 
+});
 
 
 //Exercise 4
+
+
+
+
+//Exercise 5
+
+app.get('/new-post', function (req, res) {
+  res.send(`
+  <form action="/createPost" method="POST"><!-- why does it say method="POST" ?? -->
+  <p>
+    <input type="text" name="url" placeholder="Enter a URL to content">
+  </p>
+  <p>
+    <input type="text" name="title" placeholder="Enter the title of your content">
+  </p>
+  <button type="submit">Create!</button>
+</form>`
+  );
+});
+
+//Exercise 6
 
 
 
